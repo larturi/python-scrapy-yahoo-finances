5814f04d1f2076f225ed856cf392ce1d96a14aa1
Leandro Arturi <lea.arturi@gmail.com>
2023-04-08 17:33:51 -0300

    Scrap two bonds

--- yahoo_finances/yahoo_finances/spiders/yahoo.py
+++ yahoo_finances/yahoo_finances/spiders/yahoo.py
@@ -1,5 +1,5 @@
 import scrapy
-
+    
 class SpiderYahoo(scrapy.Spider):
     name = 'yahoo'
     start_urls = ['https://finance.yahoo.com/quote/TECO2.BA']
@@ -13,17 +13,28 @@ class SpiderYahoo(scrapy.Spider):
         }
     }
     
+    # def start_requests(self):
+    #     headers = {
+    #         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    #         'Accept-Language': 'en',
+    #         'Accept-Encoding': 'gzip, deflate, br'
+    #     }
+    #     yield scrapy.Request(url='https://finance.yahoo.com/quote/TECO2.BA', headers=headers, callback=self.parse)
+
+
     def start_requests(self):
+        urls = [
+            'https://finance.yahoo.com/quote/TECO2.BA',
+            'https://finance.yahoo.com/quote/TEO',
+        ]
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-            'Accept-Language': 'en',
-            'Accept-Encoding': 'gzip, deflate, br'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
         }
-        yield scrapy.Request(url='https://finance.yahoo.com/quote/TECO2.BA', headers=headers, callback=self.parse)
-
-    # 'Volume', 'Earnings Date',
-
+        for url in urls:
+            yield scrapy.Request(url=url, headers=headers, callback=self.parse)
+            
     def parse(self, response):
+    
         keys = [
             'Previous Close',
             'Open',
@@ -48,6 +59,10 @@ class SpiderYahoo(scrapy.Spider):
         data = {}
         for i in range(len(keys)):
             data[keys[i]] = values[i]
+            
+        return {
+            'link': response.url,
+            'data': data,
+        }
 
-        return data
-        
+  
